2a9c5e7ac41bc14d25d06bfed7708d1e36956c37
is-liujun <lxzjun@qq.com>
2025-04-08 10:21:14 +0800

    initial the respository

---
+++ Step1_build/CMakeFiles/turorialApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/turorialApp.dir/tutorial.cxx.o"
+  "CMakeFiles/turorialApp.dir/tutorial.cxx.o.d"
+  "turorialApp"
+  "turorialApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turorialApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Step1_build/CMakeFiles/tutorialApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorialApp.dir/tutorial.cxx.o"
+  "CMakeFiles/tutorialApp.dir/tutorial.cxx.o.d"
+  "tutorialApp"
+  "tutorialApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorialApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
